4ab6ee10d37c61b93278bb67fa45c53e39528b28
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-03 20:39:57 +0530

    Abstract

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+abstract class Car{
+    public abstract void drive();
+    public abstract void fly();
+    public void playMusic(){
+        System.out.println("Playing music");
+    }
+}
+
+abstract class WagonR extends Car{
+    public void drive(){
+        System.out.println("Driving");
+    }
+}
+
+class UpdatedWagonR extends WagonR{
+    public void fly(){
+        System.out.println("Flying");
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Car obj = new UpdatedWagonR();
+        obj.drive();
+        obj.playMusic();
+        obj.fly();
     }
 }
