008811c2db7e1ea34e427fa499b74bd95a7f25da
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-03 23:02:32 +0530

    Interface

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
-abstract class A {
-    public abstract void show();
+interface A {
+    int age = 44;  // All the variables in interfaces final and static
+    String area = "Colombo";
+
+    void show();
+    void config();
+}
+
+class B implements A {
+    public void show(){
+        System.out.println("in show");
+    }
+    public void config(){
+        System.out.println("in config");
+    }
 }
 
     public class Main {
         public static void main(String[] args) {
-            A obj = new A(){
-                public void show(){
-                    System.out.println("in new show");
-                }
-            };
+            A obj = new B();
             obj.show();
+            obj.config();
+
+            // Static variable in interface can directly access without objects
+            System.out.println(A.area);
     }
 }
