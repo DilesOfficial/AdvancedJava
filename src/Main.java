16ac860c665db939b57d7bc93cdce34cbb4226ba
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-03 22:28:40 +0530

    Inner Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+class A {
+    int age;
+
+    public void show() {
+        System.out.println("show");
+    }
+
+    static class B {
+        public void config() {
+            System.out.println("config");
+        }
+    }
+}
+
+
+    public class Main {
+        public static void main(String[] args) {
+            A obj = new A();
+            obj.show();
+            A.B obj2 = new A.B();
+            obj2.config();
     }
 }
