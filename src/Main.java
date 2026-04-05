bc1ebf8ec173c63669764f4d8c6c9154729db254
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-05 18:44:05 +0530

    Lambda Expression

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,15 @@ interface A {
 
     public class Main {
         public static void main(String[] args) {
-            A obj = new A(){
-                public void show(){
-                    // Anonymous Inner class
-                    System.out.println("In Show");
-                }
-            };
+//            A obj = /* cut this because for lambda expression new A(){
+//                public void show*/() -> {
+//                    // Anonymous Inner class
+//                    System.out.println("In Show");
+//            };
+//            obj.show();
+
+            // Same part in line 13-18 reduced the code
+            A obj = () -> System.out.println("In Show");
             obj.show();
     }
 }
