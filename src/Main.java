c52aadbd00ea7710645167404b1fcdf30c5c3b41
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-05 18:25:44 +0530

    Functional Interface

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,21 @@
+@FunctionalInterface
 interface A {
-    int age = 44;  // All the variables in interfaces final and static
-    String area = "Colombo";
-
     void show();
-    void config();
 }
 
-class B implements A {
-    public void show(){
-        System.out.println("in show");
-    }
-    public void config(){
-        System.out.println("in config");
-    }
-}
+//class B implements A {
+//    public void show(){
+//        System.out.println("in show");
+//}
 
     public class Main {
         public static void main(String[] args) {
-            A obj = new B();
+            A obj = new A(){
+                public void show(){
+                    // Anonymous Inner class
+                    System.out.println("In Show");
+                }
+            };
             obj.show();
-            obj.config();
-
-            // Static variable in interface can directly access without objects
-            System.out.println(A.area);
     }
 }
