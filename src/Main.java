844690ac3c27a70ee8bcdfb2bb20f47fd9358069
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-06 09:53:43 +0530

    Lambda Expression Reducing Code

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,11 @@
 @FunctionalInterface
 interface A {
-    void show();
+    void show(int i);
 }
-
-//class B implements A {
-//    public void show(){
-//        System.out.println("in show");
-//}
 
     public class Main {
         public static void main(String[] args) {
-//            A obj = /* cut this because for lambda expression new A(){
-//                public void show*/() -> {
-//                    // Anonymous Inner class
-//                    System.out.println("In Show");
-//            };
-//            obj.show();
-
-            // Same part in line 13-18 reduced the code
-            A obj = () -> System.out.println("In Show");
-            obj.show();
+            A obj = i -> System.out.println("In Show " + i);
+            obj.show(5);
     }
 }
