2ccea879a8068539a6a2c7d37e5a5cfeb0a5a98b
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-06 13:59:18 +0530

    Try with Multiple Catch

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@
             int i = 0;
             int j = 0;
 
+            int nums[] = new int[5];
+            String str = null;
+
             try {
                 j = 18/i;
+                System.out.println(str.length());
+                System.out.println(nums[1]);
+                System.out.println(nums[5]);
+            }
+            catch (ArithmeticException e) {
+                System.out.println("Cannot devided by zero ");
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Stay in your limit.");
             }
             catch (Exception e) {
                 System.out.println("Something went wrong");
