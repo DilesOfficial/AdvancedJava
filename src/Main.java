e60df1932992db088e046b15a3b449aba76a87d7
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-06 14:45:18 +0530

    Ducking Exceptions using Throws

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,18 @@
-class DiluException extends Exception {
-    public DiluException(String string) {
-        super(string);
+class A {
+    public void show() throws ClassNotFoundException {
+        Class.forName("Calc");
     }
 }
-    public class Main {
-        public static void main(String[] args) {
-            int i = 20;
-            int j = 0;
-
-            try {
-                j = 18/i;
-                if (j==0) {
-                    throw new DiluException("I don't want to print zero");
-                }
-            }
-            catch (DiluException e) {
-                j = 18/1;
-                System.out.println("That the default output " + e);
-            }
-            catch (Exception e) {
-                System.out.println("Something went wrong " + e);
-            }
-            System.out.println(j);
-            System.out.println("bye");
+public class Main {
+    static {
+        System.out.println("Class Loaded");
+    }
+    public static void main(String[] args) {
+        A obj = new A();
+        try {
+            obj.show();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
