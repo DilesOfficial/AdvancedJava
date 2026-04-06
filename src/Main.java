e7a1acabd068afdbe13807337fd7f08835d66191
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-06 14:19:37 +0530

    Exception Throw

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
 
     public class Main {
         public static void main(String[] args) {
-            int i = 0;
+            int i = 20;
             int j = 0;
-
-            int nums[] = new int[5];
-            String str = null;
 
             try {
                 j = 18/i;
-                System.out.println(str.length());
-                System.out.println(nums[1]);
-                System.out.println(nums[5]);
+                if (j==0) {
+                    throw new ArithmeticException("I don't want to print zero");
+                }
             }
             catch (ArithmeticException e) {
-                System.out.println("Cannot devided by zero ");
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Stay in your limit.");
+                j = 18/1;
+                System.out.println("That the default output " + e);
             }
             catch (Exception e) {
-                System.out.println("Something went wrong");
+                System.out.println("Something went wrong " + e);
             }
             System.out.println(j);
             System.out.println("bye");
