7cca24bfb9c97bf7bc3debf47926f055564f69fe
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-07 12:00:53 +0530

    Thread Priority and Sleep

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 class A extends Thread {
     public void run() {
-        for (int i=1; i<=10; i++){
+        for (int i=1; i<=100; i++){
             System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {e.printStackTrace();}
         }
     }
 }
 
 class B extends Thread {
     public void run() {
-        for (int i=1; i<=10; i++){
+        for (int i=1; i<=100; i++){
             System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {e.printStackTrace();}
         }
     }
 }
@@ -19,6 +25,9 @@ public class Main {
         A obj1 = new A();
         B obj2 = new B();
         obj1.start();
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {e.printStackTrace();}
         obj2.start();
     }
 }
