9252c3c3f88059c9d35612c3b8f008914265c248
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-07 12:17:56 +0530

    Runnable vs Thread

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,21 @@
-class A extends Thread {
-    public void run() {
-        for (int i=1; i<=100; i++){
-            System.out.println("Hi");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {e.printStackTrace();}
-        }
-    }
-}
-
-class B extends Thread {
-    public void run() {
-        for (int i=1; i<=100; i++){
-            System.out.println("Hello");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {e.printStackTrace();}
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        A obj1 = new A();
-        B obj2 = new B();
-        obj1.start();
-        try {
-            Thread.sleep(5);
-        } catch (InterruptedException e) {e.printStackTrace();}
-        obj2.start();
+        Runnable obj1 = () -> {
+        for (int i=1; i<=5; i++){
+            System.out.println("Hi");
+            try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
+        }
+    };
+        Runnable obj2 = () -> {
+            for (int i=1; i<=5; i++){
+                System.out.println("Hello");
+                try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        t2.start();
     }
 }
